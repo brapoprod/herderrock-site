bf7ca08078c3fab91ff9b804fabfe62a34a72d39
brapoprod <broels@chili-publish.com>
2024-05-31 02:09:03 +0200

    [Fix] Add pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brapoprod.github.io',
+  base: 'herderrock-site',
   integrations: [react()]
 });
